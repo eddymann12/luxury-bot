4a9234bbf5ad3697524d0d1264dd6f98815f8b84
eddymann12 <124087432+eddymann12@users.noreply.github.com>
2025-06-26 02:11:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,6 @@ def home():
     return "Luxury Video Bot is live and ready!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=True)
+
